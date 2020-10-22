49c1500a9721a3ac2a90538c4540128d24af39bd
PeVe <paulovictor237@gmail.com>
2020-10-22 15:04:48 -0300

    MEGA UPDATE

---
+++ CMakeFiles/script_exe.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/script_exe.dir/src/comum.cpp.o"
+  "CMakeFiles/script_exe.dir/src/configdat.cpp.o"
+  "CMakeFiles/script_exe.dir/src/garra.cpp.o"
+  "CMakeFiles/script_exe.dir/src/main.cpp.o"
+  "CMakeFiles/script_exe.dir/src/matriz_pontos.cpp.o"
+  "CMakeFiles/script_exe.dir/src/pallet.cpp.o"
+  "CMakeFiles/script_exe.dir/src/pick_esteira.cpp.o"
+  "CMakeFiles/script_exe.dir/src/receita.cpp.o"
+  "script_exe"
+  "script_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/script_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
